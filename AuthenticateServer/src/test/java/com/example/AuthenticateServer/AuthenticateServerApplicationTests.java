e95586e34380a0d619225d57eb19df65b1a58097
pengshaoxiang <pengshaoxiang@bilibili.com>
2023-03-22 15:59:15 +0800

    first commit

---
+++ AuthenticateServer/src/test/java/com/example/AuthenticateServer/AuthenticateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthenticateServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
